8cc907be742af41279f4204c9a453b0e6c7ce707
Shashi Singh <shashi.we@gmail.com>
2018-05-03 09:47:57 +0530

    initial commit

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,15 @@
+class CoursesController < ApplicationController
+
+  def rails_course
+  end
+
+  def python_course
+  end
+
+  def mysql_course
+  end
+
+  def angular_course
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get '/course/full-stack-web-development-in-ruby-on-rails' => "courses#rails_course"
+  get '/course/full-stack-web-development-with-python-django' => "courses#python_course"
+  get '/course/complete-foundation-from-start-to-end-mysql' => "courses#mysql_course"
+  get '/course/complete-front-end-development-using-angular-five' => "courses#angular_course"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
