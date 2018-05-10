7b9fdeab260139cf72b97bf98efdc34c692599ed
ravi4rails <ravionrails@gmail.com>
2018-05-10 19:36:58 +0530

    quik fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  resources :course_enquiries 
+
   get '/course/full-stack-web-development-in-ruby-on-rails' => "courses#rails_course"
   get '/course/full-stack-web-development-with-python-django' => "courses#python_course"
   get '/course/complete-foundation-from-start-to-end-mysql' => "courses#mysql_course"
